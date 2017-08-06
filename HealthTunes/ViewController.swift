cb762a2169d3000da410c9eb03ee5507d56614b4
Anthony Picone <anthony@Anthonys-MacBook-Pro.local>
2017-08-06 19:04:24 -0400

    added project

---
+++ HealthTunes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HealthTunes
+//
+//  Created by Anthony Picone on 8/6/17.
+//  Copyright © 2017 Anthony Picone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
