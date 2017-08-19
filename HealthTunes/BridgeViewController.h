7a5671d0ceb505532a1805dad4e835b515723924
Matt <mattcolozzo@gmail.com>
2017-08-19 15:37:16 -0400

    spotify branch

---
+++ HealthTunes/BridgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BridgeViewController.h
+//  HealthTunes
+//
+//  Created by Matthew Colozzo on 8/19/17.
+//  Copyright © 2017 Anthony Picone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BridgeViewController : UIViewController
+
+@end
